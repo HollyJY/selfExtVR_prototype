925b6278a367d692cffbb3154068ef8f72e19655
Holly <HolllyJY@outlook.com>
2025-08-27 17:11:59 +0200

    server: local api call whisper stt

--- server/tests/1_test_stt.py
+++ server/tests/1_test_stt.py
@@ -17,27 +17,3 @@ def test_stt_smoke():
 
 if __name__ == '__main__':
     test_stt_smoke()
-
-# if __name__ == '__main__':
-#     import os, requests
-
-#     # start flask app in a separate process
-#     p = Process(target=run_flask_app)
-#     p.start()
-
-#     # wait for server to run
-#     import time
-#     time.sleep(2)
-
-#     SERVER = "http://127.0.0.1:7001"  # or internal IP
-#     INPUT_AUDIO = "tests/test_data/0_sample_audio/sample_zjy.mp3"
-
-#     resp = requests.post(
-#         f"{SERVER}/api/v1/stt",
-#         files={"audio": open(INPUT_AUDIO, "rb")},
-#         data={"session_id": "demo-session", "trial_id": "000", "lang": "en"}
-#     )
-#     print(resp.text)  # 打印原始响应内容
-#     print(resp.json())
-
-#     p.terminate()
